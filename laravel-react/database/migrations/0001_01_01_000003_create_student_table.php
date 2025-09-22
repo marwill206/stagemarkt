38c89122024636edb06b2ab843f18368a58848bf
marwill206 <laptopmerhino22@hotmail.com>
2025-09-22 10:53:41 +0200

    Add migration for creating students table with foreign key constraints

--- laravel-react/database/migrations/0001_01_01_000003_create_student_table.php
+++ laravel-react/database/migrations/0001_01_01_000003_create_student_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use PhpParser\Node\NullableType;
 
 return new class extends Migration
 {
@@ -13,6 +14,23 @@ return new class extends Migration
     {
        Schema::create('students', function (Blueprint $table) {
         $table->id('Student_ID'); // primary key
+        $table->string('Student_Name');
+        $table->string('Student_Email')->unique();
+        $table->string('Address')->nullable();
+        $table->integer('age')->nullable();
+        $table->enum('gender', ['Male', ' Female',' Other'])->nullable();
+        $table->string('foto')->nullable();
+
+        //foreign keys
+        $table->unsignedBigInteger('Profession_ID')-> nullable();
+        $table->unsignedBigInteger('School_ID')->nullable();
+        
+        $table->timestamps();
+
+
+          // FK constraints
+            $table->foreign('Profession_ID')->references('id')->on('professions')->onDelete('set null');
+            $table->foreign('School_ID')->references('id')->on('schools')->onDelete('set null');
        }) ;
 
        
