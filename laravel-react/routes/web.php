57d03c754a17348f321806ded924a597d722e46a
marwill206 <laptopmerhino22@hotmail.com>
2025-09-23 09:56:12 +0200

    Fix namespace for HomeController in route definitions

--- laravel-react/routes/web.php
+++ laravel-react/routes/web.php
@@ -1,5 +1,5 @@
 <?php
-use App\Http\Controllers\database\HomeController;
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\database\CompanyController;
 use App\Http\Controllers\database\StudentSkillController;
