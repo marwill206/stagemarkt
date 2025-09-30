23bfdfe82051b9d60b46e7c1a6602fdea2763170
marwill206 <laptopmerhino22@hotmail.com>
2025-09-30 09:26:29 +0200

    Refactor CompanyController validation rules to make Profession_ID nullable; update migration and seeder for consistency; enhance header component structure and styling

--- laravel-react/resources/js/Pages/header.jsx
+++ laravel-react/resources/js/Pages/header.jsx
@@ -1,14 +1,12 @@
-import React from 'react';
-import '../../css/app.css';
-import '../../css/style.css';
+import React from "react";
+import "../../css/app.css";
+import "../../css/style.css";
 
 export default function header() {
     return (
         <header className="header">
             <div className="header-top">
-                <div className="logo">
-                    STAGEMARKT
-                </div>
+                <div className="logo">STAGEMARKT</div>
                 <nav>
                     <a href="#">Tips</a>
                     <a href="#">FAQ</a>
@@ -19,25 +17,48 @@ export default function header() {
             <div className="header-bottom">
                 <div className="search-image"></div>
                 <div className="search">
-                    <div className="search-section">
-                        <label htmlFor="opleiding">Opleiding, bedrijfsnaam of trefwoord</label>
-                        <input type="search" className="search-input" name="opleiding"/>
+                    <div className="search-top">
+                        <div className="search-section">
+                            <label htmlFor="opleiding">
+                                Opleiding, bedrijfsnaam of trefwoord
+                            </label>
+                            <input
+                                type="search"
+                                className="search-input"
+                                name="opleiding"
+                            />
+                        </div>
+                        <div className="search-section">
+                            <label htmlFor="locatie">Plaats of postcode</label>
+                            <input
+                                type="search"
+                                className="search-input"
+                                id="input-location"
+                                name="locatie"
+                            />
+                            <select
+                                name="locatie-range"
+                                className="search-input"
+                            >
+                                <option value="0">+0km</option>
+                                <option value="5">+5km</option>
+                                <option value="10">+10km</option>
+                                <option value="20">+20km</option>
+                                <option value="50">+50km</option>
+                            </select>
+                        </div>
+                        <div className="search-section" id="search-button">
+                            <input type="submit" value="Zoeken" />
+                        </div>
                     </div>
-                    <div className="search-section">
-                        <label htmlFor="locatie">Plaats of postcode</label>
-                        <input type="search" className="search-input" id="input-location" name="locatie"/>
-                        <select name="locatie-range" className="search-input">
-                            <option value="0">+0km</option>
-                            <option value="5">+5km</option>
-                            <option value="10">+10km</option>
-                            <option value="20">+20km</option>
-                            <option value="50">+50km</option>
-                        </select>
-                    </div>
-                    <div className="search-section" id="search-button">
-                        <input type="submit" value="Zoeken" />
+                    <div className="search-bottom">
+                        <label class="switch">
+                            <input type="checkbox" />
+                            <span class="slider round"></span>
+                        </label>
                     </div>
                 </div>
+                <div></div>
             </div>
         </header>
     );
