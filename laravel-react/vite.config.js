ac56a639a1384bb6754be9e299bcee8c7932cd12
marwill206 <laptopmerhino22@hotmail.com>
2025-10-17 22:06:08 +0200

    Add HTTPS support in AppServiceProvider and configure Vite server for HTTPS

--- laravel-react/vite.config.js
+++ laravel-react/vite.config.js
@@ -5,9 +5,12 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.jsx'], // Ensure these paths are correct
+            input: ['resources/css/app.css', 'resources/js/app.jsx'],
             refresh: true,
         }),
         react(),
     ],
+    server: {
+        https: true, 
+    },
 });
